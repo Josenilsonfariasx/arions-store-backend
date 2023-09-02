b9c712eaea54c99ab97fc35ba1d75d231b45d941
Josenilsonfariasx <fariaslwork@gmail.com>
2023-09-02 14:43:32 -0300

    feat: adding routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,18 @@
 import { Router, Request, Response } from "express";
+
+// users - import
 import { CreateUserController } from "./controllers/user/CreateUserController";
+import { AuthUserController } from "./controllers/user/AuthUserController";
+import { DetailUserController } from "./controllers/user/DetailUserController";
+
+// middlewares - import
+
+import { IsAuthenticated } from "./middlewares/IsAuthenticated";
 
 export const router = Router();
 // ---Rotas---
 
 // ---user---
 router.post("/users", new CreateUserController().handle);
+router.post("/session", new AuthUserController().handle);
+router.get("/me", IsAuthenticated, new DetailUserController().handle);
